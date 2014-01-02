0afce5f7bdd58bf0fe16640f958f7ac07587970a
Hemant Gupta <hemant.gupta@apnapaisa.com>
2014-01-02 17:05:51 +0530

    added schedular projects

---
+++ Scheduling/SpringTimerTask/src/MainTest.java
@@ -0,0 +1,14 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class MainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+						  new ClassPathXmlApplicationContext("Spring-Scheduler.xml");
+	}
+
+}
