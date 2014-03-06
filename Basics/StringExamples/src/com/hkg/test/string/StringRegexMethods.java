9200cd4f7f1ec351fc357620849b8e87d3e4288c
HemantKGupta <rajni.hemant@gmail.com>
2014-03-06 20:23:34 +0530

    Added some projects in Basic

---
+++ Basics/StringExamples/src/com/hkg/test/string/StringRegexMethods.java
@@ -0,0 +1,17 @@
+package com.hkg.test.string;
+
+public class StringRegexMethods {
+
+	public static void main(String[] args) {
+		String str = "This is my small example string which I'm going to use for pattern matching.";
+		System.out.println("The string matches: "+str.matches("\\w.*"));
+		String[] splitString = (str.split("\\s+"));
+		System.out.println("The string split size is : "+splitString.length);
+		for (String string : splitString) {
+		      System.out.println(string);
+		}
+		// replace all whitespace with tabs
+		System.out.println(str.replaceAll("\\s+", "\t"));
+	}
+
+}
