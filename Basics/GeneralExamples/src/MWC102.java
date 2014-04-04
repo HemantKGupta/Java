2bac95f0cffac6cab60637a8906a91420e2870d1
HemantKGupta <rajni.hemant@gmail.com>
2014-04-05 00:38:16 +0530

    corrected some projects

---
+++ Basics/GeneralExamples/src/MWC102.java
@@ -0,0 +1,10 @@
+class MWC102 {
+  public static void main (String[] args) {
+    
+    char aa='\u0099';
+    System.out.println((int)aa);
+    char ab='\u0098';
+    System.out.println((int)ab);
+    System.out.print(aa+ab);	
+}}
+
--- Threads/MyExamples/ConcurrentLinkedDequeExample/src/com/hkg/test/concurrentdq/MainTest.java
+++ Threads/MyExamples/ConcurrentLinkedDequeExample/src/com/hkg/test/concurrentdq/MainTest.java
@@ -5,9 +5,7 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 public class MainTest {
 public static void main(String[] args) throws Exception {
 		
-		// Create a ConcurrentLinkedDeque to work with it in the example
 		ConcurrentLinkedDeque<String> list=new ConcurrentLinkedDeque<>();
-		// Create an Array of 100 threads
 		Thread threads[]=new Thread[100];
 
 		// Create 100 AddTask objects and execute them as threads
@@ -16,14 +14,13 @@ public static void main(String[] args) throws Exception {
 			threads[i]=new Thread(task);
 			threads[i].start();
 		}
-		System.out.printf("Main: %d AddTask threads have been launched\n",threads.length);
+		System.out.printf("Main: %d AddTask threads launched\n",threads.length);
 		
 		// Wait for the finalization of the threads
 		for (int i=0; i<threads.length; i++) {
 				threads[i].join();
 		}
 		
-		// Write to the console the size of the list
 		System.out.printf("Main: Size of the List: %d\n",list.size());
 		
 		// Create 100 PollTask objects and execute them as threads
@@ -32,14 +29,13 @@ public static void main(String[] args) throws Exception {
 			threads[i]=new Thread(task);
 			threads[i].start();
 		}
-		System.out.printf("Main: %d PollTask threads have been launched\n",threads.length);
+		System.out.printf("Main: %d PollTask threads launched\n",threads.length);
 		
 		// Wait for the finalization of the threads
 		for (int i=0; i<threads.length; i++) {
 			threads[i].join();
 		}
 		
-		// Write to the console the size of the list
 		System.out.printf("Main: Size of the List: %d\n",list.size());
 	}
 }
